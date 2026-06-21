c888ac0e7dfea5481bdc104616e1c19ebc0b6d0a
Aadi <aadi.2201.x.h@gmail.com>
2026-06-21 19:49:41 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canEat(vector<int>& piles, int h, int k) {
+        long long hours = 0;
+        for (int pile: piles) {
+            hours += (pile + k - 1)/k;
+        }
+        return hours <= h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int lo = 1;
+        int hi = *max_element(piles.begin(), piles.end());
+
+        int ans = hi;
+        while (lo <= hi) {
+            int mid = lo + (hi-lo)/2;
+            if (canEat(piles, h, mid)){
+                ans = mid;
+                hi = mid-1;
+            } else lo = mid+1;
+        }
+        return ans;
+    }
+};
